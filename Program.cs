f36ca5044214f50f66d2e04404d58a5a9fbf6c9b
Carlos Adriano Portes <xcap2000@outlook.com>
2022-06-03 00:08:12 -0300

    Add initial tests

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace JSP
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+            var options = new ChromeOptions();
+            // options.AddArgument("--headless");
+            // options.LeaveBrowserRunning = true;
+            // options.AddArgument("--user-data-dir=/home/carlos/.config/google-chrome/Default/Person 1");
+            // options.AddArgument("--user-data-dir=/home/carlos/.config/google-chrome/Default");
+            // options.AddArgument("--profile-directory=Person 1");
+
+            options.AddArgument("--user-data-dir=/home/carlos/.config/google-chrome/");
+            options.AddArgument("--profile-directory=Default");
+
+            using IWebDriver driver = new ChromeDriver(options);
+            driver.Navigate().GoToUrl("https://jira.jnj.com/browse/AHFY-34224");
+
+            // driver.FindElement(By.Id("username")).SendKeys("cportes");
+            // driver.FindElement(By.Id("password")).SendKeys("J3#4$5%7&8*9(");
+            // driver.FindElement(By.Id("signOnButton")).Click();
+
+            Thread.Sleep(10000);
+
+            var buttonText = driver.FindElements(By.ClassName("button")).First().Text;
+            System.Console.WriteLine(buttonText);
+
+            Thread.Sleep(10000);
+
+            // options.addArguments("--user-data-dir=C:\\Users\\user\\AppData\\Local\\Google\\Chrome\\User Data");
+            // options.addArguments("--profile-directory=Profile 2");
+
+            //driver.FindElement
+            Thread.Sleep(20000);
+
+            //username
+            //password
+            //signOnButton
+            //class button Change Device
+        }
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,7 @@
+# Jira Selenium POC
+
+```bash
+$ dotnet add package Selenium.WebDriver --version 4.2.0
+$ dotnet add package Selenium.WebDriver.ChromeDriver --version 102.0.5005.6102
+$ dotnet add package Selenium.PhantomJS.WebDriver --version 2.1.1
+```
