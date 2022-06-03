54018bab4e256fe1611b420342b6e61ad327bd02
Carlos Adriano Portes <xcap2000@outlook.com>
2022-06-03 18:39:14 -0300

    Add support

--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
 ﻿using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+// using OpenQA.Selenium.Support.UI;
 
 namespace JSP
 {
@@ -10,37 +12,28 @@ namespace JSP
             var options = new ChromeOptions();
             // options.AddArgument("--headless");
             // options.LeaveBrowserRunning = true;
-            // options.AddArgument("--user-data-dir=/home/carlos/.config/google-chrome/Default/Person 1");
-            // options.AddArgument("--user-data-dir=/home/carlos/.config/google-chrome/Default");
-            // options.AddArgument("--profile-directory=Person 1");
 
+            // Create profile manually, login in the first time, after that it'll log in automatically.
             options.AddArgument("--user-data-dir=/home/carlos/.config/google-chrome/");
-            options.AddArgument("--profile-directory=Default");
+            options.AddArgument("--profile-directory=Profile 1");
 
             using IWebDriver driver = new ChromeDriver(options);
             driver.Navigate().GoToUrl("https://jira.jnj.com/browse/AHFY-34224");
 
-            // driver.FindElement(By.Id("username")).SendKeys("cportes");
-            // driver.FindElement(By.Id("password")).SendKeys("J3#4$5%7&8*9(");
-            // driver.FindElement(By.Id("signOnButton")).Click();
+            driver.FindElement(By.Id("add-links-link")).Click();
+
+            Thread.Sleep(3000);
+
+            var selectElement = driver.FindElement(By.Id("link-type"));
+
+            var selectObject = new SelectElement(selectElement);
+            selectObject.SelectByValue("is parent task of");
+
+            var element = driver.FindElement(By.Id("jira-issue-keys-textarea"));
+            element.SendKeys("AHFY-30319");
+            element.SendKeys(Keys.Tab);
 
             Thread.Sleep(10000);
-
-            var buttonText = driver.FindElements(By.ClassName("button")).First().Text;
-            System.Console.WriteLine(buttonText);
-
-            Thread.Sleep(10000);
-
-            // options.addArguments("--user-data-dir=C:\\Users\\user\\AppData\\Local\\Google\\Chrome\\User Data");
-            // options.addArguments("--profile-directory=Profile 2");
-
-            //driver.FindElement
-            Thread.Sleep(20000);
-
-            //username
-            //password
-            //signOnButton
-            //class button Change Device
         }
     }
 }
--- Readme.md
+++ Readme.md
@@ -4,4 +4,5 @@
 $ dotnet add package Selenium.WebDriver --version 4.2.0
 $ dotnet add package Selenium.WebDriver.ChromeDriver --version 102.0.5005.6102
 $ dotnet add package Selenium.PhantomJS.WebDriver --version 2.1.1
+$ dotnet add package Selenium.Support --version 4.2.0
 ```
